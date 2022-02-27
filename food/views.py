82f22e03f5fea05d7b306631405f628dec5290db
Devyn Melendez <melend53@students.rowan.edu>
2022-02-27 14:13:50 -0500

    moved file and file indexes around

--- food/views.py
+++ food/views.py
@@ -3,14 +3,14 @@ from .forms import UploadFileForm
 
 # Create your views here.
 def home(request):
-    return render(request, 'food/food.html', {})
+    return render(request, 'home.html', {})
 
 def result(request):
-    return render(request, 'food/result.html', {})
+    return render(request, 'result.html', {})
 
 def upload_file(request):
     if request.method == 'POST':
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
             # handle_uploaded_file(request.FILES['file'])
-            return redirect('food/result.html')
+            return redirect('result.html')
