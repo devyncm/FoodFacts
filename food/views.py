42d88b8d8456bd003ec599ddef54097e4ce449a0
Devyn Melendez <melend53@students.rowan.edu>
2022-02-27 13:18:53 -0500

    starting django

---
+++ food/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, redirect
+from .forms import UploadFileForm
+
+# Create your views here.
+def home(request):
+    return render(request, 'food/food.html')
+
+def result(request):
+    return render(request, 'food/result.html')
+
+def upload_file(request):
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            # handle_uploaded_file(request.FILES['file'])
+            return redirect('food/result.html')
