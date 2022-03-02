0e33c127f19045674d6afa1fc4cbd4e279e29b7d
Devyn Melendez <melend53@students.rowan.edu>
2022-03-02 00:25:26 -0500

    image upload works and images + objects are deleted upon returning to homepage

--- food/views.py
+++ food/views.py
@@ -1,20 +1,40 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
+
+from web_project.settings import MEDIA_ROOT
 from .forms import *
+from .models import *
+
+import os
 
 # Create your views here.
-def home(request):    
+def home(request):
+    # Delete photos
+    photosdir = os.path.join(MEDIA_ROOT, 'photos')
+    for dir in os.listdir(photosdir):
+        os.remove(os.path.join(photosdir, dir))
+    # Delete photo objects
+    UploadedPhoto.objects.all().delete()
+
     if request.method == 'POST':
         form = UploadPhotoForm(request.POST, request.FILES)
 
         if form.is_valid():
             form.save()
-            request.session['id_photo'] = form.model
+            photo = UploadedPhoto.objects.last()
+            request.session['url'] = photo.photo.url
             return redirect('result')
+        # TODO: Print an error if form is not valid.
+        # TODO: Make the photo have a size requirement.
+        # TODO: Turn the photo B&W / resize if necessary for processing.
     else:
         form = UploadPhotoForm()
     return render(request, 'home.html', {'form' : form})
 
 def result(request):
     context = {}
+    if('url' in request.session):
+        context['url'] = request.session['url']
+        del request.session['url']
+
     return render(request, 'result.html', context)
